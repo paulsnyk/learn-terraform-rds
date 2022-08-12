e56aa7df8e58c370c060d51d75186a756c7b3802
paul harland <paul.harland@snyk.io>
2022-08-12 14:02:33 +0100

    remove vuln

--- main.tf
+++ main.tf
@@ -69,6 +69,6 @@ resource "aws_db_instance" "education" {
   db_subnet_group_name   = aws_db_subnet_group.education.name
   vpc_security_group_ids = [aws_security_group.rds.id]
   parameter_group_name   = aws_db_parameter_group.education.name
-  publicly_accessible    = true
+  publicly_accessible    = false
   skip_final_snapshot    = true
 }
